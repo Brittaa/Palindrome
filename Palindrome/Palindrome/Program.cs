ab59c1dd8f855a6f0282b87567d73f76c64f621b
Britta <Britta@Britta-MacBook-Air.local>
2019-10-29 16:36:40 +0200

    Palindrome

---
+++ Palindrome/Palindrome/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            Console.WriteLine("Please enter your name: ");
+            name = Console.ReadLine();
+
+            char[] ch = name.ToCharArray();
+            string rev;
+
+            Array.Reverse(ch);
+            rev = new string(ch);
+
+            bool b = name.Equals(rev, StringComparison.OrdinalIgnoreCase);
+            if(b== true)
+            {
+                Console.WriteLine($"{name} is a Palindrome!");
+            }
+            else
+            {
+                Console.WriteLine($"{name} is not a Palindrome.");
+            }
+        
+        }
+    }
+}
